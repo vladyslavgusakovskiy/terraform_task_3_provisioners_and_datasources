23fe0b449b51ea48bd3f6c3f3a60b9145e5c1b9d
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-06-18 14:05:08 +0300

    added nginx customer page, used datasource retrieve the info from resource group, added remote-exec and file provisioners

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,24 @@
 data "azurerm_resource_group" "example" {
   name = "mate-resources"
 }
+
+data "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = data.azurerm_virtual_network.main.name
+}
+
+data "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_public_ip" "example" {
+  name                = "${var.prefix}-public-ip"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
--- main.tf
+++ main.tf
@@ -1,50 +1,24 @@
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = data.azurerm_resource_group.example.location
-  resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B1s"
-
+data "azurerm_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = data.azurerm_resource_group.example.name
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   # delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
   # delete_data_disks_on_termination = true
+}
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-
+resource "null_resource" "nginx_setup" {
   provisioner "file" {
-    source      = "file.txt"
-    destination = "/home/testadmin/file.txt"
+    source      = "index.html"
+    destination = "/home/testadmin/index.html"
 
     connection {
       type     = "ssh"
       user     = "testadmin"
       password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
+      host     = data.azurerm_public_ip.example.ip_address
     }
   }
 
@@ -53,20 +27,14 @@ resource "azurerm_virtual_machine" "main" {
       type     = "ssh"
       user     = "testadmin"
       password = "Password1234!"
-      host     = azurerm_public_ip.example.ip_address
+      host     = data.azurerm_public_ip.example.ip_address
     }
 
     inline = [
       "sudo apt-get update -y",
       "sudo apt-get install -y nginx",
-      "cat /home/testadmin/file.txt"
+      "sudo mv /home/testadmin/index.html /var/www/html/index.html",
+      "sudo systemctl restart nginx"
     ]
   }
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "${var.prefix}-public-ip"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
-  allocation_method   = "Static"
 }
